a4c2240215b81dc0be5fa7f5a3e338cf00c8737d
Mark Kogon <mlkogon231@gmail.com>
2016-05-06 14:37:48 -0700

    eloquent relationships

---
+++ resources/views/cards/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+
+<h1>All Cards</h1>
+
+	@foreach ($cards as $card)
+		<div>
+			<a href="/cards/{{ $card->id }}">{{ $card->title }}</div></a>
+	@endforeach
+
+@stop
--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -3,5 +3,9 @@
 @section('content')
 
 <h1>{{ $card->title }}</h1>
-
+<ul>
+@foreach ($card->notes as $note)
+	<li>{{ $note->body }}</li>
+@endforeach
+</ul>
 @stop
